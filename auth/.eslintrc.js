04153b7a0e45e30c33bab6198b8ef1336d04c01e
TausifM <heytausifsheikh@gmail.com>
2023-07-17 14:27:08 +0530

    my first commit

---
+++ auth/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+  },
+  extends: "airbnb-typescript-prettier",
+  // // "globals": {
+  // //   "Atomics": "readonly",
+  // //   "SharedArrayBuffer": "readonly"
+  // // },
+  // // parserOptions: {
+  // //   "ecmaVersion": 2018
+  // // },
+  // // plugins: ['prettier'],
+  // rules: {
+  //   'brace-style': ['error', 'stroustrup'],
+  //   // 'comma-dangle': ['error', 'never'],
+  //   'linebreak-style': 0,
+  //   // "quotes": [2, "double"],
+  //   'no-unused-vars': 'warn',
+  // },
+};
