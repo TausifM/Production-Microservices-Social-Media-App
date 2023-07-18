1a56f69c2e3e4f075d6355b9e97da8898d96bb36
TausifM <heytausifsheikh@gmail.com>
2023-07-18 16:44:48 +0530

    Test commit

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -18,5 +18,8 @@ import app from "../../app";
 it("should return 405 for non-post requests to the signup route", async () => {});
 it("should return an error 422 if the email isnot valid", async () => {
   await request(app).post("/api/auth/signup").send({}).expect(422);
+
+
+  
   await request(app).post("/api/auth/signup").send({ email: "this is invalid email" }).expect(422);
 });
