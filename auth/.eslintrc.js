bdd25e2611d75e23e05dcb8ba988fa344850b93c
TausifM <heytausifsheikh@gmail.com>
2023-07-19 17:23:41 +0530

    final eslint set up

--- auth/.eslintrc.js
+++ auth/.eslintrc.js
@@ -3,13 +3,34 @@ module.exports = {
     "airbnb-typescript/base",
     "plugin:@typescript-eslint/recommended",
     "prettier",
+    "eslint:recommended"
   ],
+  parser: "@typescript-eslint/parser",
   plugins: ["import", "prettier", "@typescript-eslint"],
-  parserOptions: {
-    project: "./tsconfig.json",
-    // tsconfigRootDir: __dirname,
-    sourceType: "module"
-  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"], // Your TypeScript files extension
+
+      // As mentioned in the comments, you should extend TypeScript plugins here,
+      // instead of extending them outside the `overrides`.
+      // If you don't want to extend any rules, you don't need an `extends` attribute.
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        // "plugin:@typescript-eslint/recommended-requiring-type-checking"
+      ],
+
+      parserOptions: {
+        project: ["./tsconfig.json"], // Specify it only for TypeScript files
+        tsconfigRootDir: __dirname,
+        sourceType: "module"
+      }
+    }
+  ],
+  // parserOptions: {
+  //   project: "tsconfig.json",
+  //   tsconfigRootDir: __dirname,
+  //   sourceType: "module"
+  // },
   rules: {
     // quotes: [
     //   'error',
